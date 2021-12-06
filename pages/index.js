ed13e093b335f6436ed17f32c52d4d833eef14b4
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-06 11:44:57 -0700

    wirewise marketing landing page - project init

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+export default function Home() {
+  return (
+    <div>
+      <p className="j-display _300">Jakarta Display Light</p>
+      <p className="j-display _300 italic">Jakarta Display Light - Italic</p>
+      <p className="j-display _400">Jakarta Display Regular</p>
+      <p className="j-display _400 italic">Jakarta Display Regular - Italic</p>
+      <p className="j-display _500">Jakarta Display Medium</p>
+      <p className="j-display _500 italic">Jakarta Display Medium - Italic</p>
+      <p className="j-display _700">Jakarta Display Bold</p>
+      <p className="j-display _700 italic">Jakarta Display Bold - Italic</p>
+
+      <p className="j-text _200">Jakarta Text ExtraLight</p>
+      <p className="j-text _200 italic">Jakarta Text ExtraLight - Italic</p>
+      <p className="j-text _300">Jakarta Text Light</p>
+      <p className="j-text _300 italic">Jakarta Text Light - Italic</p>
+      <p className="j-text _400">Jakarta Text Regular</p>
+      <p className="j-text _400 italic">Jakarta Text Regular - Italic</p>
+      <p className="j-text _500">Jakarta Text Medium</p>
+      <p className="j-text _500 italic">Jakarta Text Medium - Italic</p>
+      <p className="j-text _600">Jakarta Text SemiBold</p>
+      <p className="j-text _600 italic">Jakarta Text SemiBold - Italic</p>
+      <p className="j-text _700">Jakarta Text Bold</p>
+      <p className="j-text _700 italic">Jakarta Text Bold - Italic</p>
+      <p className="j-text _800">Jakarta Text ExtraBold</p>
+      <p className="j-text _800 italic">Jakarta Text ExtraBold - Italic</p>
+    </div>
+  )
+}
