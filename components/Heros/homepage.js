956fce6d761a23e7fc50c6de0d8921fa0fa31646
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-14 16:26:38 -0700

    init homepage hero component and name convention

---
+++ components/Heros/homepage.js
@@ -0,0 +1,9 @@
+const HeroHome = (props) => {
+  return (
+    <>
+      <h1 className="h1 j-display 500"></h1>
+    </>
+  );
+};
+
+module.exports.Hero = HeroHome
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Header from "../components/Header";
+import { Hero } from '../components/Heros/homepage'
 import ContentWrapper from "../components/ContentWrapper";
 
 export default function Home() {
   return (
     <main className="main" id="main">
+
       <ContentWrapper>
         <Header />
+      </ContentWrapper>
+
+      <ContentWrapper>
+        <Hero />
       </ContentWrapper>
     </main>
   );
