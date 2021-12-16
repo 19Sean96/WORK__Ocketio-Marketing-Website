297384d4de6858683109111819a930806221b28d
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-16 08:55:53 -0700

    instantiated main additional pages

---
+++ pages/features/index.js
@@ -0,0 +1,9 @@
+import Header from "../../components/Header";
+
+export default function Features() {
+  return (
+    <main className="main" id="main">
+      <Header />
+    </main>
+  );
+}
---
+++ pages/pricing/index.js
@@ -0,0 +1,9 @@
+import Header from "../../components/Header";
+
+export default function Pricing() {
+  return (
+    <main className="main" id="main">
+      <Header />
+    </main>
+  );
+}
