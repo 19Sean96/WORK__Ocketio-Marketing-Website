aec0933f72832d582faec471a5e4ca2aaffca810
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-16 08:55:27 -0700

    styled and added basic funct to header navigation

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,41 @@
-
-
+import Link from "next/link";
+import ContentWrapper from "./ContentWrapper";
 
 const Header = () => {
   return (
-    <header className="header">
+    <ContentWrapper>
+      <header className="header">
         <div className="header--logo">
+          <Link href="/">
             <img src="/wirewise-logo.svg" alt="Logo for WireWise" />
+          </Link>
         </div>
         <nav className="header--nav">
-            <ul className="header--nav--list">
-                <li className="j-text _600 header--nav--item capitalize">features</li>
-                <li className="j-text _600 header--nav--item capitalize">pricing</li>
-                <li className="j-text _600 header--nav--item capitalize">contact</li>
-                {/* <li className="j-text header--nav--item">blog</li> */}
-            </ul>
+          <ul className="header--nav--list">
+            <li className="j-text _600 header--nav--item capitalize">
+              <Link href="/features">
+                <a>features</a>
+              </Link>
+            </li>
+            <li className="j-text _600 header--nav--item capitalize">
+              <Link href="/pricing">
+                <a>pricing</a>
+              </Link>
+            </li>
+            <li className="j-text _600 header--nav--item capitalize">
+              <Link href="/contact">
+                <a>contact</a>
+              </Link>
+            </li>
+            {/* <li className="j-text header--nav--item">blog</li> */}
+          </ul>
         </nav>
         {/* should change to link */}
-        <button className="header--cta btn btn--filled j-text _600 capitalize">try beta</button>
-    </header>
+        <button className="header--cta btn btn--filled j-text _600 capitalize">
+          try beta
+        </button>
+      </header>
+    </ContentWrapper>
   );
 };
 
