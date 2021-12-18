2fa64c0263fbae88ba5d4fb66a19b0f72cf3cbfd
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-17 17:27:56 -0700

    added basic responsiveness to nav  - still needs a lot of work

--- components/Header.js
+++ components/Header.js
@@ -1,39 +1,49 @@
 import Link from "next/link";
 
-const Header = () => {
+import Menu from './SVG/menu'
+
+const Header = ({ isMobile }) => {
   return (
-      <header className="header">
-        <div className="header--logo">
-          <Link href="/">
-            <img src="/wirewise-logo.svg" alt="Logo for WireWise" />
-          </Link>
-        </div>
-        <nav className="header--nav">
-          <ul className="header--nav--list">
-            <li className="j-text _600 header--nav--item capitalize">
-              <Link href="/features">
-                <a>features</a>
-              </Link>
-            </li>
-            <li className="j-text _600 header--nav--item capitalize">
-              <Link href="/pricing">
-                <a>pricing</a>
-              </Link>
-            </li>
-            <li className="j-text _600 header--nav--item capitalize">
-              <Link href="/contact">
-                <a>contact</a>
-              </Link>
-            </li>
-            {/* <li className="j-text header--nav--item">blog</li> */}
-          </ul>
-        </nav>
-        {/* should change to link */}
-        <button className="header--cta btn btn--filled j-text _600 capitalize">
-          try beta
-        </button>
-      </header>
+    <header className="header">
+      <div className="header--logo">
+        <Link href="/">
+          <img src="/wirewise-logo.svg" alt="Logo for WireWise" />
+        </Link>
+      </div>
+      <Nav isMobile={isMobile}/>
+      {/* should change to link */}
+      <button className="header--cta btn btn--filled j-text _600 capitalize">
+        try beta
+      </button>
+    </header>
   );
 };
+
+function Nav({ isMobile }) {
+
+
+  return (
+    <nav className={`header--nav${isMobile ? " header--nav__mobile" : ''}`}>
+      <ul className="header--nav--list">
+        <li className="j-text _600 header--nav--item capitalize">
+          <Link href="/features">
+            <a>features</a>
+          </Link>
+        </li>
+        <li className="j-text _600 header--nav--item capitalize">
+          <Link href="/pricing">
+            <a>pricing</a>
+          </Link>
+        </li>
+        <li className="j-text _600 header--nav--item capitalize">
+          <Link href="/contact">
+            <a>contact</a>
+          </Link>
+        </li>
+        {/* <li className="j-text header--nav--item">blog</li> */}
+      </ul>
+    </nav>
+  );
+}
 
 export default Header;
