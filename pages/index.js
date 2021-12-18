f04d1f71ca7720e1912a1937b40f2dbd3e4fa1e8
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-17 17:27:24 -0700

    added scroll, browser width & mouse event listeners to root of each page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 import Header from "../components/Header";
 import { Hero } from "../components/Heros/homepage";
@@ -9,8 +9,15 @@ import {
 } from "../components/Body/homepage";
 
 export default function Home() {
+  // Holds the X and Y value of the mouse cursor position
+  const [mouseCoord, setMouseCoord] = useState([0, 0]);
 
-  const [mouseCoord, setMouseCoord] = useState([0,0])
+  // Holds the value of the number of pixels scrolled down on the page
+  const [scrollOffset, setScrollOffset] = useState(0);
+
+  // Describes if the viewport width is mobile or desktop (875px or less = mobile)
+  const [isMobile, setMobile] = useState();
+
   const remapNum = (oldVal, oldMin, oldMax, newMin, newMax) =>
     ((oldVal - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
 
@@ -26,19 +33,42 @@ export default function Home() {
     const newMin = -1;
     const newMax = 1;
 
-
     const newX = remapNum(clientX, minX, maxX, newMin, newMax);
     const newY = remapNum(clientY, minY, maxY, newMin, newMax);
 
     setMouseCoord([newX, newY]);
   }
 
+  useEffect(() => {
+    handleResize();
 
+    function handleScroll() {
+      setScrollOffset(window.pageYOffset);
+    }
+
+    function handleResize() {
+      if (window.innerWidth <= 875) {
+        setMobile(true);
+        console.log("MOBILE");
+      } else {
+        setMobile(false);
+        console.log("DESKTOP");
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <main className="main" id="main" onMouseMove={e => handleMouseMove(e)}>
-      <Header />
-      <Hero mouseCoord={mouseCoord}/>
+    <main className="main" id="main" onMouseMove={(e) => handleMouseMove(e)}>
+      <Header isMobile={isMobile}/>
+      <Hero mouseCoord={mouseCoord} offset={scrollOffset} />
       <FirstBanner />
       <WhoSection />
       <WhatSection />
