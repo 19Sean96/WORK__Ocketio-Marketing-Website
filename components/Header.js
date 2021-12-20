ff7c707f5ec4bda5a4666a3d4bdda320c0162a4a
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-20 11:19:54 -0700

    imported scrollOffset as prop to control sticky header styling; created header wrapper element to assist in controlling styles; added "burger menu" icon for mobile viewports; added logic to Nav to control desktop/mobile styling;

--- components/Header.js
+++ components/Header.js
@@ -1,29 +1,69 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-import Menu from './SVG/menu'
+import Menu from "./SVG/menu";
 
-const Header = ({ isMobile }) => {
+const Header = ({ isMobile, scrollOffset }) => {
+  const [menuOpen, toggleMenuOpen] = useState(false);
+
   return (
-    <header className="header">
-      <div className="header--logo">
-        <Link href="/">
-          <img src="/wirewise-logo.svg" alt="Logo for WireWise" />
-        </Link>
+    <>
+      <div className="header__wrapper" style={{
+        boxShadow: scrollOffset > 0 ? '0 1px 25px -10px rgba(5,5,5, 0.25)' : '0 1px 25px -10px rgba(5,5,5, 0)'
+      }}>
+        <header className="header">
+          <div className="header--logo">
+            <Link href="/">
+              <img src="/wirewise-logo.svg" alt="Logo for WireWise" />
+            </Link>
+          </div>
+          <Nav isMobile={isMobile} menuOpen={menuOpen} />
+          {/* should change to link */}
+          {isMobile ? (
+            <button id="menuIcon" onClick={(e) => toggleMenuOpen(!menuOpen)}>
+              <Menu menuOpen={menuOpen} />
+            </button>
+          ) : (
+            <button className="header--cta btn btn--filled j-text _600 capitalize">
+              try beta
+            </button>
+          )}
+        </header>
       </div>
-      <Nav isMobile={isMobile}/>
-      {/* should change to link */}
-      <button className="header--cta btn btn--filled j-text _600 capitalize">
-        try beta
-      </button>
-    </header>
+      <span className="header__placeholder"></span>
+    </>
   );
 };
 
-function Nav({ isMobile }) {
+function Nav({ isMobile, menuOpen }) {
+  console.log((isMobile && menuOpen) || !isMobile);
+  const [navStyle, updateNavStyle] = useState();
 
+  useEffect(() => {
+    if (!isMobile) {
+      updateNavStyle({
+        transform: "translate(0)",
+        opacity: 1,
+      });
+    } else if (isMobile) {
+      if (menuOpen) {
+        updateNavStyle({
+          transform: "translate(-50%, -50%)",
+          opacity: 1,
+        });
+      } else {
+        updateNavStyle({
+          transform: "translate(-50%, -175%)",
+        });
+      }
+    }
+  }, [isMobile, menuOpen]);
 
   return (
-    <nav className={`header--nav${isMobile ? " header--nav__mobile" : ''}`}>
+    <nav
+      className={`header--nav${isMobile ? " header--nav__mobile" : ""}`}
+      style={navStyle}
+    >
       <ul className="header--nav--list">
         <li className="j-text _600 header--nav--item capitalize">
           <Link href="/features">
@@ -42,6 +82,11 @@ function Nav({ isMobile }) {
         </li>
         {/* <li className="j-text header--nav--item">blog</li> */}
       </ul>
+      {isMobile && (
+        <button className="header--cta btn btn--filled j-text _600 capitalize">
+          try beta
+        </button>
+      )}
     </nav>
   );
 }
