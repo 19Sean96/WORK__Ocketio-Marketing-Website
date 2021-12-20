4a56e7831bc58999495519261ad94137bb3a5d49
Sean Alexander <1996.Sean.Alexander@gmail.com>
2021-12-20 15:47:28 -0700

    removed unneeded comments

--- components/SVG/menu.js
+++ components/SVG/menu.js
@@ -4,11 +4,6 @@ const Menu = ({ menuOpen }) => (
     viewBox="0 0 30 23"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    // xmlns:anim="http://www.w3.org/2000/anim"
-    // anim="hover"
-    // anim:transform-origin="50% 50%"
-    // anim:duration="0.5"
-    // anim:ease="power1.inOut"
   >
     <g id="fb2c06542532917bf52a0150b903e1fc" style={{
       transform: `translateX(${menuOpen ? '4px' : '0'})`,
@@ -20,11 +15,6 @@ const Menu = ({ menuOpen }) => (
         clip-rule="evenodd"
         d="M29 3H1V1H29V3Z"
         fill="black"
-        // anim=""
-        // anim:y="0|6"
-        // anim:opacity="1|0"
-        // anim:delay="0"
-        // anim:duration="0.25"
         style={{
           opacity: menuOpen ? 0 : 1,
           transform: menuOpen ? 'translateY(6px)' : 'translateY(0)',
@@ -37,11 +27,6 @@ const Menu = ({ menuOpen }) => (
         clip-rule="evenodd"
         d="M29 9H1V7H29V9Z"
         fill="black"
-        // anim=""
-        // anim:rotation="0|40.5"
-        // anim:y="0|3"
-        // anim:delay="0.125"
-        // anim:duration="0.2"
         style={{
           transform: menuOpen ? 'rotate(40.5deg) translate(-1px, -4px)' : 'rotate(0) translate(0,0)',
           transition: '.1s all .06125s ease-in-out'
@@ -53,11 +38,6 @@ const Menu = ({ menuOpen }) => (
         clip-rule="evenodd"
         d="M29 15H1V13H29V15Z"
         fill="black"
-        // anim=""
-        // anim:rotation="0|-40.5"
-        // anim:y="0|-3"
-        // anim:delay="0.125"
-        // anim:duration="0.2"
         style={{
           transform: menuOpen ? 'rotate(-40.5deg) translate(-17px, 0px)' : 'rotate(0) translate(0,0)',
           transition: '.1s all .06125s ease-in-out'
@@ -69,11 +49,6 @@ const Menu = ({ menuOpen }) => (
         clip-rule="evenodd"
         d="M29 21H1V19H29V21Z"
         fill="black"
-        // anim=""
-        // anim:y="0|-6"
-        // anim:opacity="1|0"
-        // anim:delay="0"
-        // anim:duration="0.25"
         style={{
           opacity: menuOpen ? 0 : 1,
           transform: menuOpen ? 'translateY(-6px)' : 'translateY(0)',
