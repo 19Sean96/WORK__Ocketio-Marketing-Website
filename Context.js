883805817d9b6a4123245279056081e7faba64a7
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-01-03 20:15:38 -0700

    removed state needed for mouse position parallax effects

--- Context.js
+++ Context.js
@@ -4,9 +4,6 @@ const AppContext = createContext();
 
 export function AppWrapper({ children }) {
 
-  // Holds the X and Y value of the mouse cursor position with a range [-1,1]
-  const [mouseCoord, setMouseCoord] = useState([0, 0]);
-
   // Holds the X and Y position (in pixels) of the cursor in direct relation to the monitor
   const [mousePositionPixels, setMousePositionPixels] = useState([0,0])
 
@@ -15,27 +12,8 @@ export function AppWrapper({ children }) {
 
   // Describes if the viewport width is mobile or desktop (875px or less = mobile)
   const [isMobile, setMobile] = useState();
-
-  const remapNum = (oldVal, oldMin, oldMax, newMin, newMax) =>
-  ((oldVal - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
   
-  function handleMouseMove(e) {
-    // console.log(e.view);
-    const { clientX, clientY } = e;
-    const { innerWidth, innerHeight } = window;
-    const minX = 0;
-    const minY = 0;
-    const maxX = innerWidth;
-    const maxY = innerHeight;
-    const newMin = -1;
-    const newMax = 1;
-    
-    const newX = remapNum(clientX, minX, maxX, newMin, newMax);
-    const newY = remapNum(clientY, minY, maxY, newMin, newMax);
-    
-    setMousePositionPixels([clientX, clientY])
-    setMouseCoord([newX, newY]);
-  }
+  const handleMouseMove = ( { clientX, clientY} ) => setMousePositionPixels([clientX, clientY])
 
   useEffect(() => {
     handleResize();
@@ -66,7 +44,6 @@ export function AppWrapper({ children }) {
   return (
     <AppContext.Provider
       value={{
-        mouseCoord,
         mousePositionPixels,
         scrollOffset,
         isMobile,
--- components/Heros/homepage.js
+++ components/Heros/homepage.js
@@ -12,81 +12,81 @@ import heroImageSub3 from "../../public/images/Homepage-Landing_Hero-Sub_3.png";
 const StyledSub1 = styled(Image)`
   transform: scale(0.375)
     ${(props) =>
-      `translate(calc(-860px - 31vw - ${15 + props.mouseCoord[0] * 15}px), 
-  calc(2.5vw - ${props.offset / 7 + 15 + props.mouseCoord[1] * 15}px))`};
+      `translate(calc(-860px - 31vw), 
+  calc(2.5vw - ${props.offset / 7}px))`};
 
   @media (max-width: 1200px) {
     transform: scale(0.45)
       ${(props) =>
-        `translate(calc(-250px - 71vw - ${15 + props.mouseCoord[0] * 15}px), 
-      calc(2.5vw - ${props.offset / 7 + 15 + props.mouseCoord[1] * 15}px))`};
+        `translate(calc(-250px - 71vw), 
+      calc(2.5vw - ${props.offset / 7 + 15 + -.4 * 15}px))`};
   }
   @media (max-width: 850px) {
     transform: scale(0.485)
       ${(props) =>
-        `translate(calc(-71vw - ${15 + props.mouseCoord[0] * 15}px), 
-      calc(8.5vw - ${props.offset / 7 + 15 + props.mouseCoord[1] * 15}px))`};
+        `translate(-71vw, 
+      calc(8.5vw - ${props.offset / 7 + 15 + -.4 * 15}px))`};
   }
   @media (max-width: 575px) {
     transform: scale(0.75)
       ${(props) =>
-        `translate(calc(-41vw - ${15 + props.mouseCoord[0] * 15}px), 
-      calc(25vw - ${props.offset / 7 + 15 + props.mouseCoord[1] * 15}px))`};
+        `translate(-41vw, 
+      calc(25vw - ${props.offset / 7 + 15 + -.4 * 15}px))`};
   }
 `;
 
 const StyledSub2 = styled(Image)`
   transform: scale(0.38)
     ${(props) =>
-      `translate(calc(400px + 47.5vw - ${50 + props.mouseCoord[0] * 50}px), 
+      `translate(calc(400px + 47.5vw), 
   calc( -300px - 16vw - ${
-    props.offset / 3 + 50 + props.mouseCoord[1] * 50
+    props.offset / 3 + 50 + -.4 * 50
   }px))`};
 
   @media (max-width: 1200px) {
     transform: scale(0.46)
       ${(props) =>
-        `translate(calc(100px + 72.5vw - ${50 + props.mouseCoord[0] * 50}px), 
+        `translate(calc(100px + 72.5vw), 
       calc( -50px - 28vw - ${
-        props.offset / 3 + 50 + props.mouseCoord[1] * 50
+        props.offset / 3 + 50 + -.4 * 50
       }px))`};
   }
 
   @media (max-width: 850px) {
     transform: scale(0.54)
       ${(props) =>
-        `translate(calc(68.5vw - ${50 + props.mouseCoord[0] * 50}px), 
-      calc( -18vw - ${props.offset / 3 + 50 + props.mouseCoord[1] * 50}px))`};
+        `translate(68.5vw, 
+      calc( -18vw - ${props.offset / 3 + 50 + -.4 * 50}px))`};
   }
   @media (max-width: 575px) {
     transform: scale(0.84)
       ${(props) =>
-        `translate(calc(58.5vw - ${50 + props.mouseCoord[0] * 50}px), 
-      calc( -12vw - ${props.offset / 3 + 50 + props.mouseCoord[1] * 50}px))`};
+        `translate(58.5vw, 
+      calc( -12vw - ${props.offset / 3 + 50 + -.4 * 50}px))`};
   }
 `;
 
 const StyledSub3 = styled(Image)`
   transform: scale(1)
     ${(props) =>
-      `translate(calc(-10vw - ${50 + props.mouseCoord[0] * 5}px), 
-  calc(2.5vw - ${props.offset / -7 + 5 + props.mouseCoord[1] * 2}px))`};
+      `translate(calc(-10vw - 45px), 
+  calc(2.5vw - ${props.offset / -7 + 5 + -.4 * 2}px))`};
 
   @media (max-width: 1200px) {
     transform: scale(0.9)
       ${(props) =>
-        `translate(calc(-10vw - ${50 + props.mouseCoord[0] * 5}px), 
-      calc(2.5vw - ${props.offset / -7 + 5 + props.mouseCoord[1] * 2}px))`};
+        `translate(calc(-10vw - 45px), 
+      calc(2.5vw - ${props.offset / -7 + 5 + -.4 * 2}px))`};
   }
   @media (max-width: 850px) {
     transform: scale(0.82)
       ${(props) =>
-        `translate(calc(-15vw - ${50 + props.mouseCoord[0] * 5}px), 
-      calc(-2.5vw - ${props.offset / -7 + 5 + props.mouseCoord[1] * 2}px))`};
+        `translate(calc(-15vw - 45px), 
+      calc(-2.5vw - ${props.offset / -7 + 5 + -.4 * 2}px))`};
   }
 `;
 
-const HeroHome = ({ mouseCoord, offset }) => {
+const HeroHome = ({ offset }) => {
 
 
   return (
@@ -115,7 +115,7 @@ const HeroHome = ({ mouseCoord, offset }) => {
           </div>
         </div>
 
-        <div className="image-group" offset={offset} mouseCoord={mouseCoord}>
+        <div className="image-group" offset={offset}>
           <Image
             id="heroImageMain"
             className={`image-group__image${
@@ -133,7 +133,6 @@ const HeroHome = ({ mouseCoord, offset }) => {
             src={heroImageSub1}
             alt="this is the dashboard page. It shows user information, status and company performance statistics."
             offset={offset}
-            mouseCoord={mouseCoord}
             priority
           />
           <StyledSub2
@@ -143,7 +142,6 @@ const HeroHome = ({ mouseCoord, offset }) => {
             src={heroImageSub2}
             alt="this is the dashboard page. It shows user information, status and company performance statistics."
             offset={offset}
-            mouseCoord={mouseCoord}
             priority
           />
           <StyledSub3
@@ -154,7 +152,6 @@ const HeroHome = ({ mouseCoord, offset }) => {
             src={heroImageSub3}
             alt="this is the dashboard page. It shows user information, status and company performance statistics."
             offset={offset}
-            mouseCoord={mouseCoord}
             priority
           />
         </div>
