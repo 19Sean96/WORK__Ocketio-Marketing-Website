e08e22ce454b7eb5ac118181455afa18e1e4f681
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-01-20 15:32:05 -0700

    modified scroll animation

--- components/util/FAQ.js
+++ components/util/FAQ.js
@@ -4,11 +4,18 @@ import ScrollAnimation from "react-animate-on-scroll";
 
 const FAQList = ({ list }) => {
   return (
-    <section className="faq--list">
-      {list.map(({ question, answer }, i) => (
-        <FAQListItem question={question} answer={answer} key={i} index={i} />
-      ))}
-    </section>
+    <ScrollAnimation
+      animateIn="animate__fadeInLeft"
+      animateOut="animate__fadeOutRight"
+      duration={0.65}
+      className="faq--list__wrapper"
+    >
+      <section className="faq--list">
+        {list.map(({ question, answer }, i) => (
+          <FAQListItem question={question} answer={answer} key={i} index={i} />
+        ))}
+      </section>
+    </ScrollAnimation>
   );
 };
 
@@ -18,73 +25,66 @@ const FAQListItem = ({ question, answer, index }) => {
   const ref = useRef();
   //   console.log(ref.current.clientHeight);
   return (
-    <ScrollAnimation
-      animateIn="animate__fadeInLeft"
-      animateOut="animate__fadeOutRight"
-      duration={0.95 - index * 0.1}
-      delay={index * 150}
+    <div
+      className="faq--list--item"
+      onClick={(e) => toggleOpen(!isOpen)}
+      key={index}
     >
-      <div
-        className="faq--list--item"
-        onClick={(e) => toggleOpen(!isOpen)}
-        key={index}
-      >
-        <div className="heading">
-          <h6
-            className="h6"
-            style={{
-              color: isOpen ? "#0144e4" : "#252939",
-            }}
-          >
-            {question}
-          </h6>
-          <div
-            className="icon"
-            style={{
-              position: "relative",
-              transform: `rotate(${isOpen ? 90 : 0}deg)`,
-              transition: "all .3s ease-in-out",
-              height: "30px",
-              width: "30px",
-            }}
-          >
-            <BsChevronRight
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                fill: isOpen ? "#0144e4" : "#252939",
-                transition: "all .3s ease-in-out",
-                height: "24px",
-                width: "auto",
-              }}
-            />
-          </div>
-        </div>
-        <div
-          className="body"
+      <div className="heading">
+        <h6
+          className="h6"
           style={{
-            maxHeight: isOpen ? ref.current.clientHeight + 35 : 0,
-            transition: "max-height .4s cubic-bezier(0.33, 1, 0.68, 1)",
+            color: isOpen ? "#0144e4" : "#252939",
           }}
         >
-          <p
-            ref={ref}
-            className="p-small"
+          {question}
+        </h6>
+        <div
+          className="icon"
+          style={{
+            position: "relative",
+            transform: `rotate(${isOpen ? 90 : 0}deg)`,
+            transition: "all .3s ease-in-out",
+            height: "30px",
+            width: "30px",
+          }}
+        >
+          <BsChevronRight
             style={{
-              position: "relative",
-              transition:
-                "transform .35s cubic-bezier(.25, 1, .5, 1), opacity .25s cubic-bezier(0.64, 0, 0.78, 0)",
-              transform: `translateY(${isOpen ? 0 : 100}%)`,
-              opacity: isOpen ? 1 : 0,
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              fill: isOpen ? "#0144e4" : "#252939",
+              transition: "all .3s ease-in-out",
+              height: "24px",
+              width: "auto",
             }}
-          >
-            {answer}
-          </p>
+          />
         </div>
       </div>
-    </ScrollAnimation>
+      <div
+        className="body"
+        style={{
+          maxHeight: isOpen ? ref.current.clientHeight + 35 : 0,
+          transition: "max-height .4s cubic-bezier(0.33, 1, 0.68, 1)",
+        }}
+      >
+        <p
+          ref={ref}
+          className="p-small"
+          style={{
+            position: "relative",
+            transition:
+              "transform .35s cubic-bezier(.25, 1, .5, 1), opacity .25s cubic-bezier(0.64, 0, 0.78, 0)",
+            transform: `translateY(${isOpen ? 0 : 100}%)`,
+            opacity: isOpen ? 1 : 0,
+          }}
+        >
+          {answer}
+        </p>
+      </div>
+    </div>
   );
 };
 
