fbe8306f109ab70779bcb8d4fa26893bb3c2a5e5
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-02-10 12:37:59 -0700

    adjusted "login" button design

--- components/Header.js
+++ components/Header.js
@@ -82,7 +82,16 @@ const Header = () => {
             </button>
           ) : (
             <div className="header--cta__wrapper">
-              <a href="#" className="j-text _500" id="login" aria-label="Log in to your account here">Login</a>
+              <a href="#" className="j-text _500" id="login" aria-label="Log in to your account here" style={{
+                color: router.pathname === "/features" ||
+                router.pathname.includes("/blog") ||
+                router.pathname === '/contact' ? '#f5f5f5' : '#121212'
+              }}>Login</a>
+              <span className="pipe" style={{
+                background: router.pathname === "/features" ||
+                router.pathname.includes("/blog") ||
+                router.pathname === '/contact' ? '#f5f5f5' : '#121212'
+              }} ></span>
               <button
                 className="header--cta btn btn--filled j-text _400"
                 aria-label="Get started with the beta program"
--- components/Heros/homepage.js
+++ components/Heros/homepage.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import styled, { keyframes } from "styled-components";
 import ScrollAnimation from "react-animate-on-scroll";
@@ -9,24 +9,29 @@ import heroImageSub1 from "../../public/images/homepage/hero/hero_sub-1.png";
 import heroImageSub2 from "../../public/images/homepage/hero/hero_sub-2.png";
 import heroImageSub3 from "../../public/images/homepage/hero/hero_sub-3.png";
 
+import HomepageBG from "../SVG/HomepageBG";
+
 import { useAppContext } from "../../Context";
 const animateOnce = true;
 
 const StyledSub1 = styled(Image)`
   transform-origin: 0 100%;
   transform: scale(0.45)
-    ${(props) =>
-      `translate(-65%, calc(-40% - ${props.offset / 7}px))`};
+    ${(props) => `translate(-65%, calc(-40% - ${props.offset / 7}px))`};
 
   @media (max-width: 1200px) {
     transform: scale(0.45)
       ${(props) =>
-        `translate(-65%, calc(-40% - ${props.offset / 10 + 15 + -0.4 * 15}px))`};
+        `translate(-65%, calc(-40% - ${
+          props.offset / 10 + 15 + -0.4 * 15
+        }px))`};
   }
   @media (max-width: 850px) {
     transform: scale(0.465)
       ${(props) =>
-        `translate(-65.5%, calc(-35% - ${props.offset / 10 + 15 + -0.4 * 12}px))`};
+        `translate(-65.5%, calc(-35% - ${
+          props.offset / 10 + 15 + -0.4 * 12
+        }px))`};
   }
   @media (max-width: 575px) {
     transform: scale(0.575)
@@ -44,24 +49,30 @@ const StyledSub2 = styled(Image)`
   @media (max-width: 1200px) {
     transform: scale(0.46)
       ${(props) =>
-        `translate(60%, calc(-10% - ${props.offset / 6 + 100 + -0.4 * 150}px))`};
+        `translate(60%, calc(-10% - ${
+          props.offset / 6 + 100 + -0.4 * 150
+        }px))`};
   }
 
   @media (max-width: 850px) {
     transform: scale(0.54)
       ${(props) =>
-        `translate(60%, calc( -10% - ${props.offset / 9 + 150 + -0.4 * 150}px))`};
+        `translate(60%, calc( -10% - ${
+          props.offset / 9 + 150 + -0.4 * 150
+        }px))`};
   }
   @media (max-width: 575px) {
     transform: scale(0.7)
       ${(props) =>
-        `translate(60%, calc( -45% - ${props.offset / 12 + 50 + -0.4 * 150}px))`};
+        `translate(60%, calc( -45% - ${
+          props.offset / 12 + 50 + -0.4 * 150
+        }px))`};
   }
 `;
 
 const StyledSub3 = styled(Image)`
   transform-origin: 0 100%;
-  transform: scale(.85)
+  transform: scale(0.85)
     ${(props) =>
       `translate(-15vw, calc(1vw - ${props.offset / -7 + 5 + -0.4 * 2}px))`};
 
@@ -78,76 +89,74 @@ const StyledSub3 = styled(Image)`
   @media (max-width: 575px) {
     transform: scale(0.4)
       ${(props) =>
-        `translate(-22vw, calc(-62.5vw - ${props.offset / -16 + 5 + -0.4 * 2}px))`};
+        `translate(-22vw, calc(-62.5vw - ${
+          props.offset / -16 + 5 + -0.4 * 2
+        }px))`};
   }
 `;
 
 const StyledMain = styled(Image)`
   transform-origin: top;
-  transform: scale(.9)
-    ${(props) =>
-      ` translate(0, calc(0px - ${props.offset / -25 + -0.4}px))`};
+  transform: scale(0.9)
+    ${(props) => ` translate(0, calc(0px - ${props.offset / -25 + -0.4}px))`};
 
   @media (max-width: 1200px) {
     transform: scale(1)
-      ${(props) =>
-        ` translate(0, calc(0px - ${props.offset / -45 + -0.4}px))`};
+      ${(props) => ` translate(0, calc(0px - ${props.offset / -45 + -0.4}px))`};
   }
   @media (max-width: 850px) {
     transform: scale(1.05)
-      ${(props) =>
-        ` translate(0, calc(-12% - ${props.offset / -75 - 35}px))`};
+      ${(props) => ` translate(0, calc(-12% - ${props.offset / -75 - 35}px))`};
   }
   @media (max-width: 575px) {
     transform: scale(1.25)
-      ${(props) =>
-        ` translate(0, calc(-12% - ${props.offset / -75 - 35}px))`};
+      ${(props) => ` translate(0, calc(-12% - ${props.offset / -75 - 35}px))`};
   }
 `;
 
 const StyledImageGroup = styled.div`
   & > span {
-
     &:first-child {
-
       & > span {
-
         &::after {
-          top: 45%; left: 20%;
+          top: 45%;
+          left: 20%;
           transform-origin: top;
-          transform: scale(.9)
-          ${(props) =>
-            ` translate(0, calc(0px - ${props.offset / -20 + -0.4}px))`};
+          transform: scale(0.9)
+            ${(props) =>
+              ` translate(0, calc(0px - ${props.offset / -20 + -0.4}px))`};
 
-            @media (max-width: 1200px) {
-              top: 50.95%; left: 20%;
+          @media (max-width: 1200px) {
+            top: 50.95%;
+            left: 20%;
 
-              transform: scale(1)
-                ${(props) =>
-                  ` translate(0, calc(0px - ${props.offset / -35 + -0.4}px))`};
-            }
-            @media (max-width: 850px) {
-              top: 53%; left: 20%;
+            transform: scale(1)
+              ${(props) =>
+                ` translate(0, calc(0px - ${props.offset / -35 + -0.4}px))`};
+          }
+          @media (max-width: 850px) {
+            top: 53%;
+            left: 20%;
 
-              transform: scale(1.05)
-                ${(props) =>
-                  ` translate(0, calc(-12% - ${props.offset / -60 - 35}px))`};
-            }
-            @media (max-width: 575px) {
-              top: 63.5%; left: 20%;
+            transform: scale(1.05)
+              ${(props) =>
+                ` translate(0, calc(-12% - ${props.offset / -60 - 35}px))`};
+          }
+          @media (max-width: 575px) {
+            top: 63.5%;
+            left: 20%;
 
-              transform: scale(1.25)
-                ${(props) =>
-                  ` translate(0, calc(-12% - ${props.offset / -60 - 35}px))`};
-            }
+            transform: scale(1.25)
+              ${(props) =>
+                ` translate(0, calc(-12% - ${props.offset / -60 - 35}px))`};
+          }
         }
       }
     }
   }
-`
+`;
 
 const HeroHome = ({ offset }) => {
-
   const { isSafari } = useAppContext();
 
   return (
@@ -202,7 +211,10 @@ const HeroHome = ({ offset }) => {
           </ScrollAnimation>
         </div>
 
-        <StyledImageGroup className="image-group" offset={isSafari ? 0 : offset}>
+        <StyledImageGroup
+          className="image-group"
+          offset={isSafari ? 0 : offset}
+        >
           <StyledMain
             id="heroImageMain"
             className={`animate__animated image-group__image${
@@ -223,7 +235,6 @@ const HeroHome = ({ offset }) => {
             alt="this is the dashboard page. It shows user information, status and company performance statistics."
             offset={isSafari ? 0 : offset}
             placeholder="blur"
-
             priority
           />
           <StyledSub2
@@ -234,7 +245,6 @@ const HeroHome = ({ offset }) => {
             alt="this is the dashboard page. It shows user information, status and company performance statistics."
             offset={isSafari ? 0 : offset}
             placeholder="blur"
-
             priority
           />
           <StyledSub3
@@ -249,6 +259,10 @@ const HeroHome = ({ offset }) => {
           />
         </StyledImageGroup>
       </section>
+      <div id="homepageHeroBG">
+        <HomepageBG />
+        <div className="trailing"></div>
+      </div>
     </div>
   );
 };
