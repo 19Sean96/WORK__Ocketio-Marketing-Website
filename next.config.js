4463852a2d3904f9a08929edf62bed06958f0a2a
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-03-25 09:03:34 -0700

    added public env var for download URL

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const securityHeaders = [
     {
         key: 'Content-Security-Policy',
-        value: "default-src 'self' 'unsafe-eval' 'unsafe-inline' wirewise.io *.wirewise.io https://cms.wirewise-dev.com cms.wirewise-dev.com; img-src data: 'self' *; style-src 'self' 'unsafe-inline'; script-src-elem 'self' 'unsafe-inline' https://*.googletagmanager.com"
+        value: "default-src 'self' 'unsafe-eval' 'unsafe-inline' wirewise.io *.wirewise-dev.com *.wirewise.io https://cms.wirewise-dev.com cms.wirewise-dev.com; img-src data: 'self' *; style-src 'self' 'unsafe-inline'; script-src-elem 'self' 'unsafe-inline' https://*.googletagmanager.com"
     }
 ]
 
@@ -17,4 +17,7 @@ module.exports = {
     images: {
         domains: ['cms.wirewise-dev.com']
     },
+    env: {
+        MANAGED_DEVICE_DOWNLOAD_URL: process.env.MANAGED_DEVICE_DOWNLOAD_URL
+    }
 }
