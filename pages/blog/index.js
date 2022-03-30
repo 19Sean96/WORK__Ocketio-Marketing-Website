b7dad3ddff1fc7461266e440d09e5bf2ac749e7b
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-03-29 17:45:37 -0700

    updated root /blog page
    - Added more styled header
    - added markup for filter navigation on blog page (not functional right now)

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,64 @@
 import BlogLayout from "../../components/BlogLayout";
-import axios from 'axios'
-import Head from '../../components/Pages.Head/Blog'
+import axios from "axios";
+import Head from "../../components/Pages.Head/Blog";
+import { WaveBot } from "../../components/Site.Graphics/Waves/BlogHero";
+import ContentWrapper from "../../components/Site.Globals/ContentWrapper";
+import ScrollAnimation from "react-animate-on-scroll";
+import { useAppContext } from "../../Context";
+import Compass from "../../components/Site.Blog/Compass";
+import { useRouter } from "next/router";
+const animateOnce = true;
 export default function Blog({ blog_posts }) {
+  const { isMobile } = useAppContext();
+  const router = useRouter();
+  console.log(router);
   return (
     <>
-    <Head />
-    <main className="main" id="main">
-      <BlogLayout blogPosts={blog_posts} />
-    </main>
+      <Head />
+      <main className="main" id="blogRoot">
+        <div className="full-width_wrapper navy-bg" id="heading">
+          <ContentWrapper>
+            <Compass />
+            <section className="section section__with-grid section__with-max-width_95vw all-columns">
+              <ScrollAnimation
+                animateIn={
+                  isMobile ? "animate__fadeInUp" : "animate__fadeInDown"
+                }
+                duration={0.44}
+                offset={-186}
+                animateOnce={animateOnce}
+                className="section--heading"
+              >
+                <h2 className="h2">Blog</h2>
+                <p className="p-small">
+                  Check out our set up guides, keep up with the latest WireWise
+                  news and more
+                </p>
+              </ScrollAnimation>
+            </section>
+          </ContentWrapper>
+          <WaveBot />
+        </div>
+        <ContentWrapper>
+          <section
+            id="blogRootContent"
+            className="section section__with-grid all-columns section__with-max-width_95vw"
+          >
+            <div className="filter-row">
+              <ul className="nav-filter">
+                <li className="nav-filter__item">Tutorials</li>
+                <li className="nav-filter__item">Latest</li>
+                <li className="nav-filter__item">News</li>
+                <li className="nav-filter__item">Product Updates</li>
+              </ul>
+              <aside className="search-filter">
+                <p>live search box goes here</p>
+              </aside>
+            </div>
+          </section>
+        </ContentWrapper>
+        <BlogLayout blogPosts={blog_posts} />
+      </main>
     </>
   );
 }
@@ -27,7 +78,7 @@ export async function getStaticProps() {
       json: true,
     });
 
-    console.log(response.data)
+    console.log(response.data);
     response.status && console.log("STATUS CODE: ", response.status);
   } catch (err) {
     console.log(
