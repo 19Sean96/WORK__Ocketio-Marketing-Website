c93ff8a9333f994331d65d13527f100c19a49dc9
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-04-06 14:07:29 -0700

    updated landing page for blogging system. Also added category-specific landing pages

---
+++ pages/blog/[category].js
@@ -0,0 +1,117 @@
+import BlogLayout from "../../components/BlogLayout";
+import axios from "axios";
+
+import { WaveAlt } from "../../components/Site.Graphics/Waves/BlogHero";
+import ContentWrapper from "../../components/Site.Globals/ContentWrapper";
+import ScrollAnimation from "react-animate-on-scroll";
+import { useAppContext } from "../../Context";
+import Compass from "../../components/Site.Blog/Compass";
+import { useRouter } from "next/router";
+import Filter from "../../components/Site.Blog/Filter";
+import { useEffect, useState } from "react";
+
+const animateOnce = true;
+export default function BlogCategory({ blog_posts, params }) {
+  const [ filteredBlogList, setFilteredBlogList ] = useState([...blog_posts])
+  const [ activeCategory, setActiveCategory ] = useState(params.category)
+  const { isMobile } = useAppContext()
+
+  useEffect(() => {
+    const blogs = [...blog_posts]
+
+    if (activeCategory === 'latest') {
+      setFilteredBlogList(blogs.filter(blog => {
+        const now = new Date()
+        const blogDate = new Date(blog?.date_updated ? blog.date_updated : blog.date_created)
+
+        const diff = ( ( ( now.getTime() - blogDate.getTime() ) / 1000 ) / 60 ) / 60
+
+        return diff < ( 24 * 7 )
+      }))
+    }
+    else {
+      setFilteredBlogList(blogs.filter(blog => blog.all_categories.includes(activeCategory)))
+    }
+  }, [activeCategory, params])
+
+  return (
+    <>
+      <main className="main" id="blogRoot">
+        <div className="full-width_wrapper" id="heading">
+          <ContentWrapper>
+            <Compass />
+            <section className="section section__with-grid section__with-max-width_95vw all-columns">
+              <ScrollAnimation
+                animateIn={
+                  isMobile ? "animate__fadeInUp" : "animate__fadeInDown"
+                }
+                duration={0.44}
+                offset={-186}
+                animateOnce={animateOnce}
+                className="section--heading"
+              >
+                <h2 className="h2 capitalize">{activeCategory} Posts</h2>
+                <p className="p-small">
+                  Check out our set up guides, keep up with the latest WireWise
+                  news and more
+                </p>
+              </ScrollAnimation>
+            </section>
+          </ContentWrapper>
+          <WaveAlt />
+        </div>
+
+
+
+        <BlogLayout blogPosts={filteredBlogList} />
+      </main>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const DIRECTUS_CMS_ACCESS_KEY = process.env.DIRECTUS_CMS_ACCESS_KEY;
+  const DIRECTUS_CMS_URL = process.env.DIRECTUS_CMS_URL;
+  const blogURI = `${DIRECTUS_CMS_URL}/items/blog_categories?access_token=${DIRECTUS_CMS_ACCESS_KEY}`;
+
+  const result = await axios({
+    url: blogURI,
+    json: true
+  });
+
+  const paths = result.data.data.map(category => ({ params: { category: category.id } }))
+  return {
+      paths,
+      fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const DIRECTUS_CMS_ACCESS_KEY = process.env.DIRECTUS_CMS_ACCESS_KEY;
+  const DIRECTUS_CMS_URL = process.env.DIRECTUS_CMS_URL
+  const blogURI = `${DIRECTUS_CMS_URL}/items/all_posts`;
+ 
+  let response, error;
+  try {
+    response = await axios({
+      url: blogURI,
+      headers: {
+        Authorization: `Bearer ${DIRECTUS_CMS_ACCESS_KEY}`,
+      },
+      json: true,
+    });
+  } catch (err) {
+    error = err.response;
+  } finally {
+    if (!error) {
+      const blog_posts = response.data.data;
+
+      return {
+        props: {
+          blog_posts,
+          params
+        },
+      };
+    } else return;
+  }
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,48 +1,76 @@
 import BlogLayout from "../../components/BlogLayout";
 import axios from "axios";
 import Head from "../../components/Pages.Head/Blog";
-import { WaveBot } from "../../components/Site.Graphics/Waves/BlogHero";
+import {
+  WaveBot,
+  WaveAlt,
+} from "../../components/Site.Graphics/Waves/BlogHero";
 import ContentWrapper from "../../components/Site.Globals/ContentWrapper";
 import ScrollAnimation from "react-animate-on-scroll";
 import { useAppContext } from "../../Context";
 import Compass from "../../components/Site.Blog/Compass";
 import { useRouter } from "next/router";
 import Filter from "../../components/Site.Blog/Filter";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const animateOnce = true;
 export default function Blog({ blog_posts }) {
-  const [ filteredBlogList, setFilteredBlogList ] = useState([...blog_posts])
-  const [ activeCategory, setActiveCategory ] = useState('all')
+
+  // holds ARRAY of blog posts constrained by the "activeCategory"
+  const [filteredBlogList, setFilteredBlogList] = useState([...blog_posts]);
+  // holds STRING indicating which category of posts to show in the #blogContent
+  const [activeCategory, setActiveCategory] = useState("all");
+  // live STRING which filters which posts to show via keywords in post_title
+  const [ searchTerm, setSearchTerm ] = useState('');
+
+  const handleSearchUpdate = event => setSearchTerm(event.target.value)
+
   const { isMobile } = useAppContext();
   const router = useRouter();
 
-  console.log(filteredBlogList)
   useEffect(() => {
-    const blogs = [...blog_posts]
+    const blogs = [...blog_posts];
 
-    if (activeCategory === 'all') { setFilteredBlogList(blogs) }
-    else if (activeCategory === 'tutorials') { setFilteredBlogList(blogs.filter(blog => blog.all_categories.includes('tutorial')))}
-    else if (activeCategory === 'latest') { setFilteredBlogList(blogs.filter(blog => {
-      const now = new Date()
-      const blogDate = new Date(blog?.date_updated ? blog.date_updated : blog.date_created)
-      console.log('NOW: ', now);
-      console.log('BLOG DATE: ', blogDate, 'OLD: ', blog.date_updated)
+    if (activeCategory === "all") {
+      setFilteredBlogList(blogs);
+    } else if (activeCategory === "tutorials") {
+      setFilteredBlogList(
+        blogs.filter((blog) => blog.all_categories.includes("tutorial"))
+      );
+    } else if (activeCategory === "latest") {
+      setFilteredBlogList(
+        blogs.filter((blog) => {
+          const now = new Date();
+          const blogDate = new Date(
+            blog?.date_updated ? blog.date_updated : blog.date_created
+          );
+          console.log("NOW: ", now);
+          console.log("BLOG DATE: ", blogDate, "OLD: ", blog.date_updated);
 
-      const diff = ( ( ( now.getTime() - blogDate.getTime() ) / 1000 ) / 60 ) / 60
-      console.log(diff)
-      return diff < 6
-    }))}
-    else if (activeCategory === 'news') { setFilteredBlogList(blogs.filter(blog => blog.all_categories.includes('news')))}
-    else if (activeCategory === 'product-updates') {{ setFilteredBlogList(blogs.filter(blog => blog.all_categories.includes('product')))}}
-
-  }, [activeCategory])
+          const diff = (now.getTime() - blogDate.getTime()) / 1000 / 60 / 60;
+          console.log(diff);
+          return diff < 6;
+        })
+      );
+    } else if (activeCategory === "news") {
+      setFilteredBlogList(
+        blogs.filter((blog) => blog.all_categories.includes("news"))
+      );
+    } else if (activeCategory === "product-updates") {
+      {
+        setFilteredBlogList(
+          blogs.filter((blog) => blog.all_categories.includes("product"))
+        );
+      }
+    }
+  }, [activeCategory]);
 
   return (
     <>
       <Head />
       <main className="main" id="blogRoot">
-        <div className="full-width_wrapper navy-bg" id="heading">
+        <div className="full-width_wrapper" id="heading">
+          <WaveAlt />
           <ContentWrapper>
             <Compass />
             <section className="section section__with-grid section__with-max-width_95vw all-columns">
@@ -63,20 +91,21 @@ export default function Blog({ blog_posts }) {
               </ScrollAnimation>
             </section>
           </ContentWrapper>
-          <WaveBot />
         </div>
         <ContentWrapper>
           <section
             id="blogRootContent"
             className="section section__with-grid all-columns section__with-max-width_95vw"
           >
-            <Filter 
+            <Filter
               activeCategory={activeCategory}
               setActiveCategory={setActiveCategory}
+              searchTerm={searchTerm}
+              handleSearchUpdate={handleSearchUpdate}
             />
           </section>
         </ContentWrapper>
-        <BlogLayout blogPosts={filteredBlogList} />
+        <BlogLayout blogPosts={filteredBlogList} searchTerm={searchTerm}/>
       </main>
     </>
   );
