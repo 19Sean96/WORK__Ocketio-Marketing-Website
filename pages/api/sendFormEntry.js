4423944b12bfba59798fe282bd9138e95ea4cc2f
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-04-26 08:12:25 -0700

    posting API for form submission testing

--- pages/api/sendFormEntry.js
+++ pages/api/sendFormEntry.js
@@ -1,18 +1,26 @@
 import mailchimp_marketing from "@mailchimp/mailchimp_marketing";
-const mailchimp_transactional = require("@mailchimp/mailchimp_transactional")(
-  process.env.MAILCHIMP_KEY
-);
+const mailchimp_transactional = require("@mailchimp/mailchimp_transactional")(process.env.MANDRILL_KEY);
 export default async function handler(req, res) {
+
   mailchimp_marketing.setConfig({
     apiKey: process.env.MAILCHIMP_KEY,
     server: "us14",
   });
 
   // ADD CONTACT
+  let contact_id
   const addContactResponse = await addContact(req.body, mailchimp_marketing);
   console.log("THIS IS THE ACTUAL RESPONSE: ", addContactResponse);
 
-  // ADD TAG
+  if (addContactResponse.message === 'contact already exists') {
+
+  } else {
+    contact_id = addContactResponse.id
+  }
+
+  // // ADD TAG;
+  // const addTagResponse = await addTag(req.body, mailchimp_marketing, contact_id)
+  // console.log("THIS IS THE ADD TAG RESPONSE: ", addTagResponse);
 
   // SEND MESSAGE
   const sendMessageResponse = await sendMessage(
@@ -21,26 +29,36 @@ export default async function handler(req, res) {
   );
   console.log("THIS IS THE SEND MESSAGE RESPONSE: ", sendMessageResponse);
 
+
+  const membersResponse = await getClientInfo(
+    req.body,
+    mailchimp_marketing
+  )
+
+  // console.log("membersResponse: ", membersResponse);
+
   res.json({ message: "good to go!" });
 }
 
 async function addContact(contact, mailchimp) {
   let response;
-  console.log("DOES THE USER CONSENT TO NEWSLETTER? ", contact.newsletterOptIn);
+
+  const names = contact.fullName.split(' ')
   try {
-    response = await mailchimp.lists.addListMember("6963292142", {
+    response = await mailchimp.lists.addListMember(process.env.MAILCHIMP_MASTER_LIST_ID, {
       email_address: contact.emailInput,
       status: contact.newsletterOptIn ? "subscribed" : "pending",
       merge_fields: {
-        FNAME: contact.fullName,
-        LNAME: "",
+        FNAME: names[0],
+        LNAME: names[1] ? names[1] : '',
         EMAIL: contact.emailInput,
         PHONE: contact.phone,
       },
     });
+    response.message = 'contact added'
   } catch (err) {
     console.log("ERROR: ", err.response.body);
-    response = err.response.body;
+    response = { ...err.response.body, message: 'contact already exists'};
   }
 
   return response;
@@ -48,7 +66,6 @@ async function addContact(contact, mailchimp) {
 
 async function sendMessage(contact, mailchimp) {
   let response;
-  console.log(contact);
   try {
     response = await mailchimp.messages.send({
       message: {
@@ -59,11 +76,11 @@ async function sendMessage(contact, mailchimp) {
           contact.fullName +
           " - " +
           contact.contactType,
-        from_email: contact.emailInput,
-        from_name: process.env.MAILCHIMP_INBOX_DOMAIN,
+        from_email: 'me@seananthony.io',
+        from_name: 'Wirewise Client Site: Contact Form',
         to: [
           {
-            email: process.env.MAILCHIMP_INBOX_DOMAIN,
+            email: 'me@seananthony.io',
             name: "Mailchimp Processing",
             type: "to",
           },
@@ -71,13 +88,69 @@ async function sendMessage(contact, mailchimp) {
       },
     });
   } catch (err) {
-    console.log("ERROR: ", err.response.body);
-    response = err.response.body;
+    console.log("ERROR: ", err.response);
+    response = err.response;
   }
 
   return response;
 }
 
+async function addTag(contact, mailchimp, contact_id) {
+  let response
+
+  try {
+    response = await mailchimp.lists.updateListMemberTags(
+      MAILCHIMP_MASTER_LIST_ID,
+      contact_id,
+      { tags: [{
+        name: contact.contactType,
+        status: 'active'
+      }] }
+    )
+  }
+  catch (err) {
+    console.log('ERROR: ', err.response);
+    response = err.response
+  }
+
+  return response
+}
+
+async function getClientInfo(contact, mailchimp) {
+
+  let response , found = false
+
+  try {
+    response = await mailchimp.lists.getListMembersInfo(process.env.MAILCHIMP_MASTER_LIST_ID, {
+      count: 1000
+    })
+
+    // console.log("THIS IS THE GET CLIENT INFO RESPONSE: ", response);
+
+    response.members.map((mem, i) => {
+      // console.log('MEMBER #' + i + ': ', mem);
+
+      if (mem.full_name.toLowerCase().includes('charles')) {
+        console.log(mem);
+      }
+
+      if (mem.email_address === contact.emailInput) {
+        console.log('CONTACT HAS BEEN FOUND!')
+        console.log(mem);
+        found = true
+      }
+    })
+
+    console.log('FOUND? ', found);
+  }
+  catch (err) {
+    console.log('ERROR: ', err.response);
+    response = err.response
+  }
+
+  return response
+}
+ 
 /*** FORM SUBMISSION PROCESS - NOTES ***/
 /*
     - Standard contact form at '/contact' - add the following:
