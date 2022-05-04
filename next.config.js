32787fdd8c6eed04d5bc76d2c3b933f13a4dff7b
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-05-04 09:39:40 -0700

    test 1: amplify build process - error with sib-api-sdk package build

--- next.config.js
+++ next.config.js
@@ -1,23 +1,31 @@
 const securityHeaders = [
-    {
-        key: 'Content-Security-Policy',
-        value: "default-src 'self' 'unsafe-eval' 'unsafe-inline' wirewise.io *.wirewise-dev.com *.wirewise.io https://cms.wirewise-dev.com cms.wirewise-dev.com; img-src data: 'self' *; style-src 'self' 'unsafe-inline'; script-src-elem 'self' 'unsafe-inline' https://*.googletagmanager.com https://*.google-analyics.com https://*.google.com https://*.doubleclick.net"
-    }
-]
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self' 'unsafe-eval' 'unsafe-inline' wirewise.io *.wirewise-dev.com *.wirewise.io https://cms.wirewise-dev.com cms.wirewise-dev.com; img-src data: 'self' *; style-src 'self' 'unsafe-inline'; script-src-elem 'self' 'unsafe-inline' https://*.googletagmanager.com https://*.google-analyics.com https://*.google.com https://*.doubleclick.net",
+  },
+];
 
 module.exports = {
-    // async headers() {
-    //     return [
-    //         {
-    //             source: '/(.*)',
-    //             // headers: securityHeaders
-    //         }
-    //     ]
-    // },
-    images: {
-        domains: ['cms.wirewise-dev.com']
+  // async headers() {
+  //     return [
+  //         {
+  //             source: '/(.*)',
+  //             // headers: securityHeaders
+  //         }
+  //     ]
+  // },
+  images: {
+    domains: ["cms.wirewise-dev.com"],
+  },
+  env: {
+    MANAGED_DEVICE_DOWNLOAD_URL: process.env.MANAGED_DEVICE_DOWNLOAD_URL,
+  },
+  rules: [
+    {
+      parser: {
+        amd: false,
+      },
     },
-    env: {
-        MANAGED_DEVICE_DOWNLOAD_URL: process.env.MANAGED_DEVICE_DOWNLOAD_URL
-    }
-}
+  ],
+};
