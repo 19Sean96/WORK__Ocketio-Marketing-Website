a3e22a609fb801bab1332d762cb1f70d431bd317
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-05-09 12:15:53 -0700

    issue: 504 error when requesting API from /pages/api - test #1 to attempt resolution

--- components/Pages.Body/Contact.js
+++ components/Pages.Body/Contact.js
@@ -25,7 +25,7 @@ const ContactPortal = (props) => {
     formState: { errors },
   } = useForm();
   const onSubmit = async (data) => {
-    const result = await fetch('/api/sibVanillaApi', {
+    const result = await fetch('/api/sendinblue', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
