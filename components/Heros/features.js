ac48e26ca43dec09ca4fe921ed713841047b3976
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-05-25 15:47:28 -0700

    updated responsive styles for features/homepage

--- components/Heros/features.js
+++ components/Heros/features.js
@@ -15,14 +15,42 @@ const animateOnce = true;
 const StyledHeroLeft = styled(HeroLeft)`
   color: green;
   transform: scale(1)
-    ${props => ` translate(9%, calc(0px - ${props.offset / -10 + -1}px))`}
+    ${props => ` translate(9%, calc(0px - ${props.offset / -10 + -1}px))`};
+
+  @media (max-width: 1150px) {
+    transform: scale(1)
+    ${props => ` translate(22.5%, calc(-25px - ${props.offset / -10 + -1}px))`}
+  }
+  @media (max-width: 875px) {
+    transform: scale(1)
+    ${props => ` translate(-8%, calc(-25px - ${props.offset / -10 + -1}px))`}
+  }
+
+  @media (max-width: 875px) {
+    transform: scale(1)
+    ${props => ` translate(-50%, calc(-25px - ${props.offset / -10 + -1}px))`}
+  }
 
 `
 
 const StyledHeroRight = styled(HeroRight)`
   color: red;
   transform: scale(1)
-  ${props => ` translate(${-(props.offset / -25)}px, calc(0px - ${props.offset / -15 + -1}px))`}
+  ${props => ` translate(${-(props.offset / -25)}px, calc(0px - ${props.offset / -15 + -1}px))`};
+
+  @media (max-width: 1150px) {
+    transform: scale(1)
+    ${props => ` translate(calc(25px + ${-(props.offset / -15)}px), calc(25px - ${props.offset / -15 + -1}px))`};
+  }
+  @media (max-width: 875px) {
+    transform: scale(1)
+    ${props => ` translate(calc(35px + ${-(props.offset / -15)}px), calc(-10% - ${props.offset / -15 + -1}px))`};
+  }
+
+  @media (max-width: 450px) {
+    transform: scale(1)
+    ${props => ` translate(calc(30% + ${-(props.offset / -15)}px), calc(0% - ${props.offset / -15 + -1}px))`};
+  }
 `
 
 const HeroFeatures = ({ offset }) => {
