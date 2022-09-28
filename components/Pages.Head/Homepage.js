34fed77b6d61f3638152018367182f15128186b3
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-09-28 14:10:55 -0700

    updated homepage opengraph image ref

--- components/Pages.Head/Homepage.js
+++ components/Pages.Head/Homepage.js
@@ -1,5 +1,4 @@
 import { NextSeo } from "next-seo";
-import Logo from '../../public/ocketio-logo.svg'
 
 const Head = () => {
   const DIRECTUS_CMS_URL = process.env.DIRECTUS_CMS_URL;
@@ -16,7 +15,7 @@ const Head = () => {
           "Wirewise is a streamlined VPN that facilitates remote access to networks you manage. Set up across devices and add users in minutes.",
         images: [
           {
-            url: Logo,
+            url: THIS_URL + '/ocketio-logo.svg',
             width: 800,
             height: 600,
             alt: "Preview of the Wirewise Admin Panel - where you can manage devices and administer Tenant management",
