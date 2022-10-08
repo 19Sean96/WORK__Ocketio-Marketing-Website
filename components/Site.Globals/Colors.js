90ea7c893ddfd2fa684bba542bb7da5587330472
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-10-07 20:32:08 -0700

    added list of color vars for CSS/JS reference

---
+++ components/Site.Globals/Colors.js
@@ -0,0 +1,31 @@
+const _primarywhite = '#f5f5f5';
+const _primaryblack = '#101010';
+const _secondarywhite = '#fdfdfd';
+const _secondaryblack = '#181c1c';
+const _mint = '#88E4C3';
+const _palemint = '#C3CCCC';
+const _pastelred = '#EC5563';
+const _bluelight = '#339BFC';
+const _bluemid = '#151779';
+const _bluedark = '#030430';
+const _gray1 = '#333';
+const _gray2 = '#666';
+const _gray3 = '#999';
+const _gray4 = '#ccc';
+
+export {
+    _primarywhite,
+    _primaryblack,
+    _secondarywhite,
+    _secondaryblack,
+    _mint,
+    _palemint,
+    _pastelred,
+    _bluelight,
+    _bluemid,
+    _bluedark,
+    _gray1,
+    _gray2,
+    _gray3,
+    _gray4
+}
