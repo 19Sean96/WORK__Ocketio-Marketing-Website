e7b571e72c0805d3089e35562a79481286d321ad
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-10-12 00:17:21 -0700

    added new params to ContentWrapper for functionality

--- components/Site.Globals/ContentWrapper.js
+++ components/Site.Globals/ContentWrapper.js
@@ -1,6 +1,7 @@
-const ContentWrapper = ({ children, width }) => {
-    return <div className="main_wrapper" style={{
-        maxWidth: width || 1382
+const ContentWrapper = ({ children, width, padBot=undefined, id=null }) => {
+    return <div id={id || null} className="main_wrapper" style={{
+        maxWidth: width || 1382,
+        paddingBottom: padBot || 'inset'
     }}>{children}</div>;
   };
   export default ContentWrapper;
