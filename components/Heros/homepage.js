a9356c39313928b3e86d33235fe83f3ca8cd8e8e
Sean Alexander <1996.Sean.Alexander@gmail.com>
2022-10-13 13:38:16 -0700

    overhauled homepage hero section + "What" section

--- components/Heros/homepage.js
+++ components/Heros/homepage.js
@@ -9,64 +9,77 @@ import { useAppContext } from "../../Context";
 const animateOnce = true;
 
 const StyledImageGroup = styled.div`
+
+
+
   #heroImageMain {
-    transform-origin: top;
+    transform-origin: bottom left;
     transform: scale(1)
-      ${(props) => ` translate(0, calc(0px - ${props.offset / -25 + -0.4}px))`};
+      ${(props) => ` translate3d(0, calc(0px - ${props.offset / -20 + -0.4}px), 0)`};
   }
 
   #heroImageSub {
-    top: 30%;
+    top: 40%;
     left: 20%;
     transform-origin: top;
     width: 46vw;
     max-width: 666px;
     transform: scale(1)
-      ${(props) => ` translate(0, calc(0px - ${props.offset / -20 + -0.4}px))`};
+      ${(props) => ` translate3d(0, calc(0px - ${props.offset / -15 + -0.4}px), 0)`};
   }
 
   @media screen and (max-width: 1200px) {
     #heroImageMain {
       transform: scale(1)
-        ${(props) => ` translate(0, calc(0px - ${props.offset / -45 + -31}px))`};
+        ${(props) => ` translate3d(0, calc(0px - ${props.offset / -25 + -31}px), 0)`};
     }
 
     #heroImageSub {
-      top: 37%;
+      top: 50%;
       left: 18%;
       transform: scale(.95)
-        ${(props) => ` translate(0, calc(0px - ${props.offset / -35 + -22}px))`};
+        ${(props) => ` translate3d(0, calc(0px - ${props.offset / -20 + -22}px), 0)`};
     }
   }
 
   @media screen and (max-width: 1100px) {
+    #heroImageMain {
+      transform: scale(1)
+        ${(props) => ` translate3d(0, calc(0px - ${props.offset / -30 + -31}px), 0)`};
+    }
     #heroImageSub {
-      top: 41%;
+      top: 72%;
+      left: 33%;
+      transform: scale(1.45)
+        ${(props) => ` translate3d(0, calc(0px - ${props.offset / -25 + -22}px), 0)`};
     }
   }
 
   @media screen and (max-width: 850px) {
     #heroImageMain {
-      transform: scale(1.25)
-        ${(props) => ` translate(0, calc(-12% - ${props.offset / -75 - 35}px))`};
+      transform: scale(1.2)
+        ${(props) => ` translate3d(-10%, calc(50px - ${props.offset / -35 - 35}px), 0)`};
     }
 
     #heroImageSub {
-      top: 46%;
-      left: 17%;
-      transform: scale(0.98)
-        ${(props) => ` translate(0, calc(-12% - ${props.offset / -60 - 46}px))`};
+      top: 58%;
+      left: 32%;
+      transform: scale(1.65)
+        ${(props) => ` translate3d(0, calc(-12% - ${props.offset / -30 - 46}px), 0)`};
     }
   }
 
   @media screen and (max-width: 575px) {
-    #heroImageSub {
-      top: 37.5%;
-      left: 16%;
+    #heroImageMain {
+      transform: scale(1.1)
+        ${(props) => ` translate3d(-7%, calc(50px - ${props.offset / -40 - 35}px), 0)`};
+    }
 
-      transform: scale(0.95)
-        ${(props) =>
-          ` translate(0, -50% + calc(-6vw - ${props.offset / -60 - 63}px))`};
+    #heroImageSub {
+      top: 65%;
+      left: 35%;
+      transform: scale(1.75)
+        ${(props) => ` translate3d(0, calc(-12% - ${props.offset / -35 - 46}px), 0)`};
     }
   }
 `;
